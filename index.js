fe8261a7e9f57dc7266d42d15e0f0e4051f05b27
K.Kivi <72075784+kivik-beep@users.noreply.github.com>
2022-02-12 03:01:07 +0200

    3.9 puhelinluettelon backend step9

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '));
@@ -86,7 +88,7 @@ app.get('/', (req, res) => {
     response.json(person)
   })
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
